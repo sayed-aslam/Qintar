fa874d9dcdec49c5320f521340943380af19239c
Syed Aslam <sayedaslam593@gmail.com>
2024-01-07 22:39:12 +0530

    nav changes

---
+++ navbarScroll.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 200) { // Adjust the scroll position where you want the effect to start (e.g., 200 pixels)
+        navbar.classList.add('bg-opacity-75');
+    } else {
+        navbar.classList.remove('bg-opacity-75');
+    }
+});
+
