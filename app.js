6be615a3795a44cdd7528f0cfb6d03b5513d929d
Syed Aslam <sayedaslam593@gmail.com>
2024-01-10 11:43:18 +0530

    no changes

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+
+// JavaScript to toggle mobile menu visibility
+const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
